2333599e9bf7d6447212c9a4f04816a70ec14a0d
Hatem Alfarra <hma194@Usask.ca>
2023-05-30 18:47:53 -0400

    Skeleton of welcome message, game rules, and accepting a file name as an input

---
+++ Question 3 Conway's game of life.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+
+
+print() # Welcome message
+print() # Rules
+
+
+def Conway(input_textfile):
+    return
+
+
+
+while True:
+
+    input_textfile = input('Text file name: ')
+    f = open(input_textfile, 'r')
+
